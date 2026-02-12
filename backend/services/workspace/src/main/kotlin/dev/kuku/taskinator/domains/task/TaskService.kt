0b9cee3bf1e7e8b79529b7142ae3a27190fe304e
kuchuk-borom-debbarma <kuchukboromd@gmail.com>
2026-02-13 05:01:20 +0530

    Defined based functions for task service

--- backend/services/workspace/src/main/kotlin/dev/kuku/taskinator/domains/task/TaskService.kt
+++ backend/services/workspace/src/main/kotlin/dev/kuku/taskinator/domains/task/TaskService.kt
@@ -2,13 +2,19 @@ package dev.kuku.taskinator.domains.task
 
 import org.springframework.stereotype.Service
 
-data class TaskToCreate(
+data class TaskToCreateParam(
     val name: String,
     val description: String?,
+    val projectId: String,
     val parentTaskId: String?,
     val assignedTeam: String?,
     val assignedMember: String?
 )
+
+
+data class AssignTaskParam(val teamId: String?, val teamMemberId: String?)
+
+data class GetTasksFilterParam()
 
 @Service
 /**
@@ -23,5 +29,21 @@ data class TaskToCreate(
  * No custom status for now lets keep things simple
  */
 interface TaskService {
-    fun createTask(projectId: String, userId: String, toCreate: TaskToCreate)
+    /// Create a task or sub-task for a project
+    fun createTask(projectId: String, userId: String, toCreate: TaskToCreateParam)
+
+    /// Assign a task to a team and/or team-member
+    fun assignTask(projectId: String, userId: String, taskId: String, assignTo: AssignTaskParam)
+
+    /// Update the status of a given task. Any team member can update the task if no member assigned, if member is assigned then only that person can.
+    fun updateTaskStatus(projectId: String, userId: String, taskId: String, status: String)
+
+    /// Delete a task. Sub tasks will have to be deleted in async manner too
+    fun deleteTask(projectId: String, userId: String, taskId: String)
+
+    /// get the task by Id as long as authorized
+    fun getTaskById(projectId: String, userId: String): ProjectTask?
+
+    /// Get tasks based on given filter as long as authorized
+    fun getTasks(projectId: String, userId: String, filter: GetTasksFilterParam): List<ProjectTask>
 }
