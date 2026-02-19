b2a878fdcc27dbada25ab71b821af8802b6f7f8c
kuchuk-borom-debbarma <kuchukboromd@gmail.com>
2026-02-19 15:57:00 +0530

    fixed url

--- backend/services/identity/src/util/url.ts
+++ backend/services/identity/src/util/url.ts
@@ -2,7 +2,5 @@ import { Context } from "hono";
 
 export const getVerificationLink = (c: Context, pathSuffix: string, token: string) => {
   const url = new URL(c.req.url);
-  const currentPath = c.req.path;
-  const basePath = currentPath.substring(0, currentPath.lastIndexOf("/"));
-  return `${url.origin}${basePath}/${pathSuffix}?token=${token}`;
+  return `${url.origin}/api/v1/public/${pathSuffix}?token=${token}`;
 };
