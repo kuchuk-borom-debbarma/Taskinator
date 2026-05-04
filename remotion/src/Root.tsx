b9b59804f381412decf943855bdbfa78d996b647
kuchuk-borom-debbarma <45627511+kuchuk-borom-debbarma@users.noreply.github.com>
2026-05-04 19:21:43 +0530

    feat: add SyncArchitecture composition to visualize task creation flow

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -3,6 +3,7 @@ import { Composition } from "remotion";
 import { MyComposition } from "./Composition";
 import { FeatureShowcase } from "./FeatureShowcase";
 import { SchemaDesign } from "./SchemaDesign";
+import { SyncArchitecture } from "./SyncArchitecture";
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -19,6 +20,14 @@ export const RemotionRoot: React.FC = () => {
         id="SchemaDesign"
         component={SchemaDesign}
         durationInFrames={3360}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+      <Composition
+        id="SyncArchitecture"
+        component={SyncArchitecture}
+        durationInFrames={1620}
         fps={30}
         width={1280}
         height={720}
